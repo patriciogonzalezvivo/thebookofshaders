8ec1ef741efe42af160de5da7e9945142f800133
Patricio Gonzalez Vivo <patriciogonzalezvivo@gmail.com>
2015-08-31 15:11:49 -0400

    adding UX details

--- chap-header.php
+++ chap-header.php
@@ -3,7 +3,7 @@
 	echo '
 	<div class="header">
 		<p><a href="http://patriciogonzalezvivo.com/2015/thebookofshaders/">The Book of Shaders</a> by <a href="http://patriciogonzalezvivo.com">Patricio Gonzalez Vivo</a> </p>
-		<p>Translations: <a href=".">English</a> - <a href="?lan=jp">Japanese</a></p>
+		<p>Translations: <a href=".">English</a> - <a href="?lan=jp">日本人</a></p>
 	</div>
 	<hr>
 	';
