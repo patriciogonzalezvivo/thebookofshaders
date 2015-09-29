4ee5ef607ecdd89f6468d60caf4798711dcfd976
Patricio Gonzalez Vivo <patriciogonzalezvivo@gmail.com>
2015-09-29 07:34:25 -0400

    details

--- chap-header.php
+++ chap-header.php
@@ -2,8 +2,8 @@
 
 	echo '
 	<div class="header">
-		<p><a href="http://patriciogonzalezvivo.com/2015/thebookofshaders/">The Book of Shaders</a> by <a href="http://patriciogonzalezvivo.com">Patricio Gonzalez Vivo</a> </p>
-		<p>Translations: <a href=".">English</a> - <a href="?lan=jp">日本語</a></p>
+		<p><a href="http://thebookofshaders.com/">The Book of Shaders</a> by <a href="http://patriciogonzalezvivo.com">Patricio Gonzalez Vivo</a> </p>
+		<p><a href=".">English</a> - <a href="?lan=jp">日本語</a></p>
 	</div>
 	<hr>
 	';
