30c02df1689efafb9ff4cda68f4740f3b5bdcb5d
Patricio Gonzalez Vivo <patriciogonzalezvivo@gmail.com>
2016-11-15 08:37:50 -0500

    deuthc

--- toc-header.php
+++ toc-header.php
@@ -1,3 +1,3 @@
 <div class="toc-header">
-    <p> <a href="?lan=jp">日本語</a> - <a href="?lan=ch">中文版</a> - <a href="?lan=kr">한국어</a> - <a href="?lan=es">Español</a> - <a href="?lan=fr">Français</a> - <a href="?lan=it">Italiano</a>  - <a href=".">English</a></p>
+    <p> <a href="?lan=jp">日本語</a> - <a href="?lan=ch">中文版</a> - <a href="?lan=kr">한국어</a> - <a href="?lan=es">Español</a> - <a href="?lan=fr">Français</a> - <a href="?lan=it">Italiano</a> - <a href="?lan=de">Deutsch</a> - <a href=".">English</a></p>
 </div>
