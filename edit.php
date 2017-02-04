ebb58da840ba70729d0924493b0f15a7b2d6b74c
Patricio Gonzalez Vivo <patriciogonzalezvivo@users.noreply.github.com>
2017-02-04 16:56:10 -0500

    Update edit.php

--- edit.php
+++ edit.php
@@ -54,7 +54,6 @@
     //     <meta name="twitter:image:width" content="500">
     //     <meta name="twitter:image:height" content="500">';
 ?>
-        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 		<style>
 			body {
 				height: 100%;
