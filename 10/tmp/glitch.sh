529a84cb2fef4c28632aef44f707875419cf8d60
Patricio Gonzalez Vivo <patriciogonzalezvivo@gmail.com>
2017-03-20 09:37:22 -0400

    adding pixel spirit

--- 10/tmp/glitch.sh
+++ 10/tmp/glitch.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-FILE=$1
-SEC=$2
+SHADER=$1
+SEC=$1
 
 COUNTER=0
 for i in `seq -w 0.01 .031 $SEC`; do
 	echo $i
-    `glslViewer img-glitch.frag --u_tex $FILE -s $i -o frame-$COUNTER.png`
+    `glslViewer $SHADER -s $i -o frame-$COUNTER.png`
     let COUNTER=COUNTER+1 
 done
 
