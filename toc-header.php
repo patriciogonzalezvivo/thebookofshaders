1ac1f5654fe54026ad42624ee67f18d7413d2d75
Hoang.Vu <vuphuonghoang88@gmail.com>
2020-07-30 14:02:29 +0700

    Add Vietnamese option

--- toc-header.php
+++ toc-header.php
@@ -1,3 +1,3 @@
 <div class="toc-header">
-    <p> <a href="?lan=jp">日本語</a> - <a href="?lan=ch">中文版</a> - <a href="?lan=kr">한국어</a> - <a href="?lan=es">Español</a> - <a href="?lan=pt">Portugues</a> - <a href="?lan=fr">Français</a> - <a href="?lan=it">Italiano</a> - <a href="?lan=de">Deutsch</a> - <a href="?lan=ru">Русский</a> - <a href=".">English</a></p>
+    <p> <a href="?lan=vi">Tiếng Việt</a> - <a href="?lan=jp">日本語</a> - <a href="?lan=ch">中文版</a> - <a href="?lan=kr">한국어</a> - <a href="?lan=es">Español</a> - <a href="?lan=pt">Portugues</a> - <a href="?lan=fr">Français</a> - <a href="?lan=it">Italiano</a> - <a href="?lan=de">Deutsch</a> - <a href="?lan=ru">Русский</a> - <a href=".">English</a></p>
 </div>
